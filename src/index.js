1b8b96a0c75cea9fbf52fa219fc51fee8994ac8a
lucy2n <lyusianaumenko@yandex.by>
2023-11-11 19:35:06 +0300

    fix: refactor toggleButton method

--- src/index.js
+++ src/index.js
@@ -12,42 +12,22 @@ const borders = page.querySelectorAll('.project')
 const toggleButton = () => {
     themeButton.addEventListener("click", () => {
         themeButton.classList.toggle('header__button_theme_dark')
-        if(themeButton.classList.contains('header__button_theme_dark')) {
-            page.classList.add('page_theme_dark')
-            logo.classList.add('header__logo_theme_dark')
-            headerLinks.forEach((item) => {
-                item.classList.add('header__link_theme_dark')
-            })
-            projectLinks.forEach((item) => {
-                item.classList.add('project__link_theme_dark')
-            })
-            borders.forEach((item) => {
-                item.classList.add('project_theme_dark')
-            })
-            footerLinks.forEach((item) => {
-                item.classList.add('footer__link_theme_dark')
-            })
-            arrowImage.classList.add('about__image_theme_dark')
-        } else {
-            page.classList.remove('page_theme_dark')
-            logo.classList.remove('header__logo_theme_dark')
-            headerLinks.forEach((item) => {
-                item.classList.remove('header__link_theme_dark')
-            })
-            borders.forEach((item) => {
-                item.classList.remove('project_theme_dark')
-            })
-            projectLinks.forEach((item) => {
-                item.classList.remove('project__link_theme_dark')
-            })
-            footerLinks.forEach((item) => {
-                item.classList.remove('footer__link_theme_dark')
-            })
-            arrowImage.classList.remove('about__image_theme_dark')
-        }
+        page.classList.toggle('page_theme_dark')
+        logo.classList.toggle('header__logo_theme_dark')
+        headerLinks.forEach((item) => {
+            item.classList.toggle('header__link_theme_dark')
+        })
+        projectLinks.forEach((item) => {
+            item.classList.toggle('project__link_theme_dark')
+        })
+        borders.forEach((item) => {
+            item.classList.toggle('project_theme_dark')
+        })
+        footerLinks.forEach((item) => {
+            item.classList.toggle('footer__link_theme_dark')
+        })
+        arrowImage.classList.toggle('about__image_theme_dark')
     })
 }
-
-
 
 toggleButton();
